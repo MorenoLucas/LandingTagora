abcee5c39bb0ea6f3a92accfa6615f624e64653e
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-07-07 17:58:36 +0200

    componentes feed, calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    FeedComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
